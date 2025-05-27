8e564d88b8f83227e12447bef76e43f8bafa896e
Vivekananda Reddy Godala <godalavivekananda.in@gmail.com>
2025-05-27 11:52:24 +0530

    Initialised Navbar and setup project structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default async function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <main className="font-work-sans">
+            <Navbar />
+            {children}
+        </main>
+    );
+}

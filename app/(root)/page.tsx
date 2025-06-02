37598c5fc067a5eb102a97bdbde9650bd918dd52
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2025-06-02 12:29:12 +0530

    feat: startup cards now load instantly via Sanity Live integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import StartupCard, { StartupCardType } from "../../components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 
 export default async function Home({searchParams}: {
@@ -9,7 +9,10 @@ export default async function Home({searchParams}: {
 }) {
   const query = (await searchParams).query
 
-  const posts = await client.fetch(STARTUPS_QUERY)
+
+  const { data: posts } = await sanityFetch({
+    query: STARTUPS_QUERY
+  });
 
   console.log(JSON.stringify(posts, null, 2))
 
@@ -40,7 +43,7 @@ export default async function Home({searchParams}: {
       </ul>
 
    </section>
-
+    <SanityLive/>
    </>
   );
 }

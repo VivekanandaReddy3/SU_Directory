da6648c46604b0025d6047709fe21b134525a046
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2025-06-07 08:14:10 +0200

    Fix GROQ query param missing error

--- components/View.tsx
+++ components/View.tsx
@@ -3,7 +3,8 @@ import Ping from "./Ping"
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries"
 
 const View = async ({id}:{id:string}) => {
-    const {views: totalViews} = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {params: {id}});
+    const result = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id});
+    const totalViews = result?.views || 0;
   return <div className="view-container">
     <div className="absolute -top-2 -right-2">
         <Ping/>

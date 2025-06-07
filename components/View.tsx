d72776ad9bb39060b753b8a7ea2ed34e35c1f762
VivekanandaReddy3 <godalavivekananda.in@gmail.com>
2025-06-07 08:41:55 +0200

    Initialise Sanity write client

--- components/View.tsx
+++ components/View.tsx
@@ -5,6 +5,9 @@ import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries"
 const View = async ({id}:{id:string}) => {
     const result = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id});
     const totalViews = result?.views || 0;
+
+
+    //TODO: Update the number of Views
   return <div className="view-container">
     <div className="absolute -top-2 -right-2">
         <Ping/>
@@ -12,7 +15,7 @@ const View = async ({id}:{id:string}) => {
 
     <p className="view-text">
         <span className="font-black">
-            {totalViews} views
+            {totalViews} {totalViews === 1 ? 'View' : 'Views'}
         </span>
     </p>
 
